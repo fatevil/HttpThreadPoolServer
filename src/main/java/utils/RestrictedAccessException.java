a3ac110e00b5f6fcf7e5f9724084ce54c8735e53
fatevil <marekoka@seznam.cz>
2016-05-22 15:38:01 +0200

    Acces exception added, checking permission implemented.

---
+++ src/main/java/utils/RestrictedAccessException.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Created by marek on 22.5.16.
+ */
+public class RestrictedAccessException extends Exception {
+    public RestrictedAccessException(String message) {
+        super(message);
+    }
+}
