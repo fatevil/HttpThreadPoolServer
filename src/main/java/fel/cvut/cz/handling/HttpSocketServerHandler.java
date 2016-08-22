21cfbf3bcf0a51954a439c011aac0ce68b70685b
fatevil <marekoka@seznam.cz>
2016-08-22 15:45:26 +0200

    Javadoc comments created for HttpExchangeService.

--- src/main/java/fel/cvut/cz/handling/HttpSocketServerHandler.java
+++ src/main/java/fel/cvut/cz/handling/HttpSocketServerHandler.java
@@ -4,10 +4,18 @@ import fel.cvut.cz.server.HttpSocketServerRequest;
 import fel.cvut.cz.server.HttpSocketServerResponse;
 
 /**
+ * Implementation of this class is supposed to respond to HTTP requests.
+ * <p>
  * Created by marek on 27.5.16.
  */
 public interface HttpSocketServerHandler {
 
+    /**
+     * Solves HTTP request and makes response.
+     *
+     * @param httpSocketServerRequest  request specifics
+     * @param httpSocketServerResponse response mediator
+     */
     void handle(HttpSocketServerRequest httpSocketServerRequest, HttpSocketServerResponse httpSocketServerResponse);
 
 }
